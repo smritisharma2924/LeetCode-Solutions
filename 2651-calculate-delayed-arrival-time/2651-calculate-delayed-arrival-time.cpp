ae876512b50cd7266b7f2b852b041d965dcb3285
Smriti Sharma <smritisharma2905@gmail.com>
2026-06-26 00:39:57 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.97%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int time = arrivalTime + delayedTime;
+        if (time < 24) return time;
+        else return time-24;
+    }
+};
